a5586c755125298e9b2868900985ec1dc70da5a2
ChrisOtt0 <sophusott@gmail.com>
2023-02-06 20:35:26 +0100

    Assignment complete

--- main.py
+++ main.py
@@ -1,20 +1,48 @@
-import RPi as GPIO
+import RPi.GPIO as GPIO
+import random
 import time
 
+unit = 1
+input_port = 18
+outs = [22, 13, 26, 23, 12, 20]
 GPIO.setmode(GPIO.BCM)
 
+def blink():
+    for p in outs:
+        GPIO.output(p, False)
+
+    ports = []
+    for i in range(0, 6):
+        x = random.randint(0, 1)
+        if x == 1:
+            ports.append(outs[i])
+
+    for p in ports:
+        GPIO.output(p, True)
+
+    time.sleep(unit / 4)
+
+    for p in ports:
+        GPIO.output(p, False)
+    
+
 def roll_dice():
-    print("You pressed the button")
-    GPIO.output(26, True)
-    time.sleep(1)
-    GPIO.output(26, False)
+    for i in range(0, 6):
+        blink()
+        time.sleep(unit / 8)
 
-GPIO.setup(26, GPIO.OUT)
-GPIO.setup(18, GPIO.IN, pull_up_down = GPIO.PUD_UP)
-GPIO.add_event_detect(18, GPIO.FALLING, callback = roll_dice)
+    x = random.randint(1, 6)
+    i = 0
+    while i < x:
+        GPIO.output(outs[i], True)
+        i += 1
 
-i = 0
+GPIO.setup(outs, GPIO.OUT)
+GPIO.setup(input_port, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+
 while True:
-    i += 1
-    print(i)
-    time.sleep(1)
+    pressed = GPIO.input(input_port)
+    if not pressed:
+        roll_dice()
+
+GPIO.cleanup()
